3a091534cf812e2ede5a52fe237d876edd02ba37
nguyenvangithub <81097647+nguyenvangithub@users.noreply.github.com>
2022-05-11 21:52:59 +0700

    update vặt

--- src/main/java/com/java/repository/SaveRepository.java
+++ src/main/java/com/java/repository/SaveRepository.java
@@ -13,12 +13,13 @@ public interface SaveRepository extends JpaRepository<Save, Integer> {
 	@Query(value = "SELECT * FROM SAVES where book_id  = ? and customerId = ?;", nativeQuery = true)
 	public Save selectSaves(Integer bookId, String customerId);
 	
-	// Hiển thị Top 3 sách mới nhất
+	// dsSachs đc yeu thich nhất của KH theo Id
 	@Query(value = "SELECT * FROM SAVES where customerId = ?;", nativeQuery = true)
 	public List<Save> selectAllSaves(String customerId);
 
-	// Hiển thị Top 3 sách mới nhất
+	// sỗ sách yêu thích nhất  theo id kahch Hang
 	@Query(value = "SELECT Count(id)  FROM book_store.saves  where  customerId = ?;", nativeQuery = true)
 	public Integer selectCountSave(String customerId);
+	
 
 }
